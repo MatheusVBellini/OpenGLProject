89a4a1a142753fad8c73befccef2443da3296e8e
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-10-18 13:54:54 -0300

    new cmake

---
+++ cmake-build-debug/CMakeFiles/OpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLProject.dir/include/vendor/stb_image.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/include/vendor/stb_image.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/main.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/main.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Application.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Application.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/FileParser.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/FileParser.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/GObject.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/GObject.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/GObjectFactory.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/GObjectFactory.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Renderer.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Shader.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/VertexBuffer.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/VertexBuffer.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Window.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Window.cpp.o.d"
+  "OpenGLProject"
+  "OpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
