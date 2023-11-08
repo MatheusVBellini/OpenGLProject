b590000fa11968bef91e2a93585f5d33c1169ea4
MatheusVBellini <matheus.bellini02@usp.br>
2023-11-08 08:08:56 -0300

    using steve

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 int main() {
     // boot-up
     Application app = Application::getInstance();
-    Window window(800, 800, "Hello World");
+    Window window(1600, 1600, "Hello World");
     Renderer ren;
     app.bindRenderer(ren);
     app.bindWindow(window);
@@ -24,7 +24,7 @@ int main() {
     GObject skull = GObjectFactory::genObjectFromFile("skull", "skull.jpg");
     GObject penguin = GObjectFactory::genObjectFromFile("penguin", "penguin.png");
     GObject head = GObjectFactory::genObjectFromFile("head", "head.jpg");
-    GObject hand = GObjectFactory::genObjectFromFile("hand", "hand.png");
+    GObject steve = GObjectFactory::genObjectFromFile("Steve", "Steve.jpg");
     GObject elephant = GObjectFactory::genObjectFromFile("elephant", "elephant.png");
 
     // controller instantiation
@@ -35,7 +35,7 @@ int main() {
     /* PROJECT FUNCTIONS */
 
     // show Nth object when key N is pressed
-    std::array<GObject*,5> objects = {&skull, &penguin, &head, &hand, &elephant};
+    std::array<GObject*,5> objects = {&skull, &penguin, &head, &steve, &elephant};
     for (int i = 0; i < objects.size(); i++) {
         con.setKeyFunc(std::to_string(i+1), [&window, &con, &objects, i](int,int,int action,int,GObject* previous){
             if (!action) return;
--- src/control/modules/ManualMov.cpp
+++ src/control/modules/ManualMov.cpp
@@ -34,12 +34,11 @@ bool ManualMov::checkBoundaries(const glm::mat4& mat, const GObject* obj) {
     bool value = true;
     glm::vec4 boundary = {0,0,0,0};
 
-    // translation validation
     for (auto& current : obj->getHitbox()) {
         boundary = mat * glm::vec4(current,1);
         if ((boundary.x > 1) || (boundary.x < -1)) value = false;
         if ((boundary.y > 1) || (boundary.y < -1)) value = false;
-        if ((boundary.z > 1) || (boundary.z < -1)) value = false;
+        //if ((boundary.z > 1) || (boundary.z < -1)) value = false;
     }
 
     return value;
